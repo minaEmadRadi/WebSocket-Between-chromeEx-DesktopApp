b19c158497b2ff1c8abbfc91b4c2eeca2296f2d7
mina emad radi <majisa96@outlook.com>
2023-12-18 13:57:01 +0200

    'UI-dotnet'

---
+++ WebSocketDesktopUi/WebSocketDesktopUi/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WebSocketDesktopUi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(2, 3);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(204, 172);
+            this.txtLog.TabIndex = 0;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(2, 181);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(204, 20);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(62, 207);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 235);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtLog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ WebSocketDesktopUi/WebSocketDesktopUi/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WebSocketDesktopUi
+{
+    public partial class Form1 : Form
+    {
+        private HttpListener httpListener;
+        private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private WebSocket connectedClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartWebSocketServer();
+        }
+
+        private async void StartWebSocketServer()
+        {
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:8080/");
+            httpListener.Start();
+
+            while (!cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                var context = await httpListener.GetContextAsync();
+                if (context.Request.IsWebSocketRequest)
+                {
+                    var webSocketContext = await context.AcceptWebSocketAsync(null);
+                    var webSocket = webSocketContext.WebSocket;
+                    HandleWebSocket(webSocket);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    context.Response.Close();
+                }
+            }
+        }
+
+        private async void HandleWebSocket(WebSocket webSocket)
+        {
+            connectedClient = webSocket;
+            var buffer = new byte[1024 * 4];
+
+            try
+            {
+                while (webSocket.State == WebSocketState.Open)
+                {
+                    var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    if (result.MessageType == WebSocketMessageType.Text)
+                    {
+                        var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+                        // Call the new method to process and respond to the message
+                        txtLog.AppendText("Received: " + message + Environment.NewLine);
+
+                    }
+                    else if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                Invoke((MethodInvoker)delegate
+                {
+                    txtLog.AppendText("Error: " + ex.Message + Environment.NewLine);
+                });
+            }
+            finally
+            {
+                if (webSocket != null)
+                {
+                    webSocket.Dispose();
+                }
+                connectedClient = null;
+            }
+        }
+
+        
+
+
+
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connectedClient != null && connectedClient.State == WebSocketState.Open)
+                {
+                    string message = txtMessage.Text;
+                    if (string.IsNullOrWhiteSpace(message))
+                    {
+                        MessageBox.Show("Please enter a message to send.");
+                        return;
+                    }
+
+                    byte[] buffer = Encoding.UTF8.GetBytes(message);
+
+                    await connectedClient.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                    txtLog.AppendText("Sent: " + message + Environment.NewLine);
+                    txtMessage.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("No client is connected or the connection is not open.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            cancellationTokenSource.Cancel();
+            httpListener?.Stop();
+        }
+    }
+
+}
